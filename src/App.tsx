3608325bf128755c9dd2589db873595499b32e1f
pmuzychenko <pmuzychenko@gmail.com>
2020-11-29 12:41:55 +0200

    feat: add functions for changing values into input elements

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,22 @@ function App() {
     let [maxValue, setMaxValue] = useState<number>(5)
     let [minValue, setMinValue] = useState<number>(0)
 
+    const changeMaxValue = ( value: number) => {
+        setMaxValue(value)
+    }
+    const changeMinValue = ( value: number) => {
+        setMinValue(value)
+    }
+
+
     return (
         <div className="App">
             <Settings maxValueTitle={maxValueTitle}
                       startValueTitle={startValueTitle}
                       maxValue={maxValue}
                       minValue={minValue}
+                      changeMaxValue={changeMaxValue}
+                      changeMinValue={changeMinValue}
 
             />
             <Button title={'set'}/>
--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ChangeEvent } from 'react';
 import './App.css';
 
 type PropsType = {
@@ -6,18 +7,34 @@ type PropsType = {
     startValueTitle: string
     maxValue: number
     minValue: number
+    changeMaxValue: (value: number) => void
+    changeMinValue: (value: number) => void
 }
 
 function Settings(props: PropsType) {
+
+    const setMaxValue = (e:ChangeEvent<HTMLInputElement>) => {
+        props.changeMaxValue(+e.currentTarget.value)
+    }
+    const setMinValue = (e:ChangeEvent<HTMLInputElement>) => {
+        props.changeMinValue(+e.currentTarget.value)
+    }
+
+
     return (
         <div className="settings">
             <div className={"maxValue"}>
                 <span> {props.maxValueTitle} </span>
-                <input type="number" value={props.maxValue}/>
+                <input type="number" value={props.maxValue}
+                       onChange={setMaxValue}
+
+                />
             </div>
             <div className={"startValue"}>
                 <span>{props.startValueTitle} </span>
-                <input type="number" value={props.minValue}/>
+                <input type="number" value={props.minValue}
+                       onChange={setMinValue}
+                />
             </div>
         </div>
     );
