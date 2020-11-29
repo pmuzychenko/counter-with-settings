b2f232a7a856ebb364942e3d4b5f96611c71a6bf
pmuzychenko <pmuzychenko@gmail.com>
2020-11-29 12:16:54 +0200

    feat: add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Button from './Button';
 import Settings from "./Settings";
 
 function App() {
     return (
         <div className="App">
             <Settings/>
+            <Button />
         </div>
     );
 }
---
+++ src/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Button() {
+    return (
+        <div>
+            <button>SET</button>
+        </div>
+    );
+}
+
+export default Button;
